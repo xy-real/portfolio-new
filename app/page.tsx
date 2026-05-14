1fc4f35c2a7c08e7542dcb33d14c6577245b0abd
Xyryll Jay Taneo <xyrylljay@gmail.com>
2026-05-14 23:51:08 +0800

    feat: update media query for light color scheme and restructure page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,35 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+
+      <div>
         {/* Header content */}
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-to-br before:from-transparent before:to-cyan-200 before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:bg-gradient-conic before:from-sky-200 before:via-transparent before:dark:from-cyan-900 before:dark:via-[#0141ff] before:dark:blur-2xl before:dark:content-[''] after:blur-2xl after:content-[''] after:dark:from-cyan-900 after:dark:via-[#0141ff]">
+      <div>
         {/* Hero content */}
       </div>
 
-      <div className="relative z-[-1] flex flex-wrap items-center justify-center gap-4">
-        {/* Content grid */}
+      <div>
+        {/* About Me */}
       </div>
+
+      <div>
+        {/* Skills & Technologies */}
+      </div>
+
+      <div>
+        {/* Projects */}
+      </div>
+
+      <div>
+        {/* Achievements */}
+      </div>
+
+      <div>
+        {/* Contact */}
+      </div>
+      
     </main>
   );
 }
